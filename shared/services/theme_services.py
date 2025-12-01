5e8f023981727f9310835e21af122fb43c9d69d3
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 15:09:14 -0400

    Update theme_services.py

--- shared/services/theme_services.py
+++ shared/services/theme_services.py
@@ -9,7 +9,7 @@ from shared.models.theme_model import (
     DEFAULT_THEME,
     generate_theme_css,
 )
-from app.utils.exeptions import NotFoundException
+from app.utils.exceptions import NotFoundException
 
 
 async def get_company_theme(
