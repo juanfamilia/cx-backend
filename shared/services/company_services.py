6d9fcf98e74d2d8a27553f40fbab572fdd77a900
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 15:45:32 -0400

    Fix import path for NotFoundException

--- shared/services/company_services.py
+++ shared/services/company_services.py
@@ -12,7 +12,7 @@ from shared.models.company_model import (
     CompanyUpdate,
 )
 from shared.types.pagination import Pagination
-from app.utils.exceptions import NotFoundException
+from shared.utils.exceptions import NotFoundException
 
 
 async def get_companies(
