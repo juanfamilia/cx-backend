1d3226d867e9920b64f58a1386989a29f93d86ed
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 14:51:34 -0400

    Fix import statement for PermissionDeniedException

--- app/routes/campaign_assignment_router.py
+++ app/routes/campaign_assignment_router.py
@@ -8,7 +8,7 @@ from shared.services.campaign_assignment_services import (
     get_assiments_campaigns,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
