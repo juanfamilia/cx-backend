44845ff86876371e437c4208bc12f48a337963b9
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 14:38:31 -0400

    Fix import path for InvalidTokenException

--- app/core/security.py
+++ app/core/security.py
@@ -6,7 +6,7 @@ from jwt.exceptions import DecodeError
 from passlib.context import CryptContext
 
 from app.core.config import settings
-from app.utils.exeptions import InvalidTokenException
+from app.utils.exceptions import InvalidTokenException
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
