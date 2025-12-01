90bd3bc0b737b064d8f576b9ccf049f2dbec0bf4
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 15:11:07 -0400

    Fix import path for NotFoundException

--- shared/services/zone_services.py
+++ shared/services/zone_services.py
@@ -4,7 +4,7 @@ from sqlmodel import select
 
 from shared.models.user_zone_model import UserZone
 from shared.models.zone_model import Zone, ZonePublic
-from app.utils.exeptions import NotFoundException
+from app.utils.exceptions import NotFoundException
 
 
 async def get_zones(session: AsyncSession) -> List[ZonePublic]:
