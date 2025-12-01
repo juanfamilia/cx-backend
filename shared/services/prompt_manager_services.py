e48f493188bc63e5de5be437b8e280202ef3e8b6
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 15:47:57 -0400

    Update prompt_manager_services.py

--- shared/services/prompt_manager_services.py
+++ shared/services/prompt_manager_services.py
@@ -9,7 +9,7 @@ from shared.models.prompt_manager_model import (
     PromptManagerPublic,
     PromptManagersPublic,
 )
-from app.utils.exceptions import NotFoundException
+from shared.utils.exceptions import NotFoundException
 
 
 async def get_prompt_by_id(
