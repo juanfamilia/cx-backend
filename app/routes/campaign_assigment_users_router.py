1d8b01b5e48ced00d31c520a8d3fa8e17f19902c
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-01 14:40:51 -0400

    Fix import path for PermissionDeniedException

--- app/routes/campaign_assigment_users_router.py
+++ app/routes/campaign_assigment_users_router.py
@@ -16,7 +16,7 @@ from shared.services.campaign_assignment_services import (
     soft_delete_campaign_users,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
