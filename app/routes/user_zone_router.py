af27607d2bac8120b9916bea3ebf408783b0bca0
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-02 15:53:00 -0400

    Update user_zone_router.py

--- app/routes/user_zone_router.py
+++ app/routes/user_zone_router.py
@@ -16,7 +16,7 @@ from shared.services.user_zone_services import (
     update_user_zone,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
