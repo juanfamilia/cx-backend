193ebf675c1ff14612b36fd3d57ff3978351b1a9
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-02 15:45:02 -0400

    Update payment_router.py

--- app/routes/payment_router.py
+++ app/routes/payment_router.py
@@ -17,7 +17,7 @@ from shared.services.payment_services import (
     update_payment,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
