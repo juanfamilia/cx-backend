eb60d88ceb2ed26693c44050e0c0db6cb86454f7
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-02 15:52:04 -0400

    Update survey_router.py

--- app/routes/survey_router.py
+++ app/routes/survey_router.py
@@ -17,7 +17,7 @@ from shared.services.survey_forms_services import (
     update_survey_form,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
