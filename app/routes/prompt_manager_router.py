6d1500c7b9e01dd2c9f05776aaf1cafac29b0d64
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-02 15:51:47 -0400

    Update prompt_manager_router.py

--- app/routes/prompt_manager_router.py
+++ app/routes/prompt_manager_router.py
@@ -16,7 +16,7 @@ from shared.services.prompt_manager_services import (
     soft_delete_prompt,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
