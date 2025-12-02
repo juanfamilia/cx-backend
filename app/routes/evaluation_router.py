a8c9b38b1fdd9a2b513ac68a5b73fa0f3efe67bb
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-02 15:38:28 -0400

    Update evaluation_router.py

--- app/routes/evaluation_router.py
+++ app/routes/evaluation_router.py
@@ -40,7 +40,7 @@ from shared.services.video_services import (
     update_video_status,
 )
 from shared.utils.deps import check_company_payment_status, get_auth_user
-from shared.utils.exeptions import PermissionDeniedException
+from shared.utils.exceptions import PermissionDeniedException
 
 
 router = APIRouter(
