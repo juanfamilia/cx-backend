68b60482d69c00fca64998c9f6e7d76907238534
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-03 12:58:16 -0400

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -14,7 +14,7 @@ from app.routes import (
     dashboard_router,
     dashboard_config_router,
     evaluation_router,
-    #evaluation_analysis_router,
+    # evaluation_analysis_router,
     notification_router,
     payment_router,
     prompt_manager_router,
@@ -28,26 +28,26 @@ from app.routes import (
 api_router = APIRouter()
 
 api_router.include_router(widget_router)
-api_router.include_router(auth_router.router)
-api_router.include_router(user_router.router)
-api_router.include_router(company_router.router)
-api_router.include_router(payment_router.router)
-api_router.include_router(zone_router.router)
-api_router.include_router(user_zone_router.router)
-api_router.include_router(survey_router.router)
-api_router.include_router(campaign_router.router)
-api_router.include_router(campaign_assignment_router.router)
-api_router.include_router(campaign_assigment_users_router.router)
-api_router.include_router(campaign_assigment_zones_router.router)
-api_router.include_router(evaluation_router.router)
-api_router.include_router(notification_router.router)
-api_router.include_router(dashboard_router.router)
-api_router.include_router(dashboard_config_router.router)
-api_router.include_router(cloudflare_router.router)
-api_router.include_router(cloudflare_webhook_router.router)
-#api_router.include_router(evaluation_analysis_router.router)
-api_router.include_router(campaign_goals_evaluator_router.router)
-api_router.include_router(campaign_goals_progress_router.router)
-api_router.include_router(prompt_manager_router.router)
-# api_router.include_router(intelligence_router.router)
-api_router.include_router(theme_router.router)
+api_router.include_router(auth_router)
+api_router.include_router(user_router)
+api_router.include_router(company_router)
+api_router.include_router(payment_router)
+api_router.include_router(zone_router)
+api_router.include_router(user_zone_router)
+api_router.include_router(survey_router)
+api_router.include_router(campaign_router)
+api_router.include_router(campaign_assignment_router)
+api_router.include_router(campaign_assigment_users_router)
+api_router.include_router(campaign_assigment_zones_router)
+api_router.include_router(evaluation_router)
+api_router.include_router(notification_router)
+api_router.include_router(dashboard_router)
+api_router.include_router(dashboard_config_router)
+api_router.include_router(cloudflare_router)
+api_router.include_router(cloudflare_webhook_router)
+# api_router.include_router(evaluation_analysis_router)
+api_router.include_router(campaign_goals_evaluator_router)
+api_router.include_router(campaign_goals_progress_router)
+api_router.include_router(prompt_manager_router)
+# api_router.include_router(intelligence_router)
+api_router.include_router(theme_router)
