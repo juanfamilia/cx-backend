f0eb8b7fe129608535e270c6f856cda450368850
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-03 12:05:01 -0400

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -14,7 +14,7 @@ from .company_router import router as company_router
 from .dashboard_router import router as dashboard_router
 from .dashboard_config_router import router as dashboard_config_router
 from .evaluation_router import router as evaluation_router
-from .intelligence_router import router as intelligence_router
+#from .intelligence_router import router as intelligence_router
 from .notification_router import router as notification_router
 from .payment_router import router as payment_router
 from .prompt_manager_router import router as prompt_manager_router
@@ -23,4 +23,5 @@ from .theme_router import router as theme_router
 from .user_router import router as user_router
 from .user_zone_router import router as user_zone_router
 from .zone_router import router as zone_router
+
 
