efd5f4555b991964745b372fe6a6b8db5d78caf0
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2025-12-22 18:12:47 -0400

    Update zone_router.py

--- app/routes/zone_router.py
+++ app/routes/zone_router.py
@@ -2,8 +2,8 @@ from typing import List
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from shared.core.db import get_db
-from shared.models.zone_model import ZonePublic
-from shared.services.zone_services import get_limit_zones, get_zone, get_zones
+from shared.models.zone_model import ZoneBase,ZonePublic
+from shared.services.zone_services import get_limit_zones, get_zone, get_zones,create_zone
 from shared.utils.deps import check_company_payment_status, get_auth_user
 
 
@@ -13,6 +13,16 @@ router = APIRouter(
     dependencies=[Depends(get_auth_user), Depends(check_company_payment_status)],
 )
 
+
+@router.post("/", response_model=ZonePublic)
+async def create_zone_endpoint(
+    data: ZoneBase,
+    request: Request,
+    session: AsyncSession = Depends(get_db),
+) -> ZonePublic:
+    # según tu diseño puedes usar company_id o user_id del request.state.user
+    zone = await create_zone(session=session, data=data, user=request.state.user)
+    return zone
 
 @router.get("/")
 async def get_all(
