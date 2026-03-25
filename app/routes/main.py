e68bf5b207e78c020d556437612e9554333a2721
juanfamilia <45854567+juanfamilia@users.noreply.github.com>
2026-03-25 13:54:25 -0400

    Fix include_router call for auth_router

--- app/routes/main.py
+++ app/routes/main.py
@@ -27,7 +27,7 @@ from app.routes import (
 
 api_router = APIRouter()
 
-api_router.include_router(auth_router.router)
+api_router.include_router(auth_router)
 api_router.include_router(user_router.router)
 api_router.include_router(company_router.router)
 api_router.include_router(payment_router.router)
